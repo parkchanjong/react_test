b36ad2b4cd1779f7d1e364bec97f96359fd75956
parkchanjong <7000cj@gmail.com>
2020-04-25 01:13:13 +0900

    ### [react client test]  - client

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Profile from "./Profile";
+
+function App() {
+  return (
+    <div>
+      <Profile username="chanjong" name="박찬종" />
+    </div>
+  );
+}
+
+export default App;
