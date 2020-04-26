a8f0c1455af1af569efdd1464f2b8b7ac9cbd6e3
parkchanjong <7000cj@gmail.com>
2020-04-26 14:08:34 +0900

    [add test code] - reat test

--- src/Profile.test.js
+++ src/Profile.test.js
@@ -4,7 +4,18 @@ import Profile from "./Profile";
 
 describe("<Profile />", () => {
   it("matches snapshot", () => {
-    const wrapper = mount(<Profile username="parkchanjong" name="박찬종" />);
+    const wrapper = mount(<Profile username="chanjong" name="박찬종" />);
     expect(wrapper).toMatchSnapshot();
+  });
+  it("renders username and name", () => {
+    const wrapper = mount(<Profile username="chanjong" name="박찬종" />);
+
+    expect(wrapper.props().username).toBe("chanjong");
+    expect(wrapper.props().name).toBe("박찬종");
+
+    const boldElement = wrapper.find("b");
+    expect(boldElement.contains("chanjong")).toBe(true);
+    const spanElement = wrapper.find("span");
+    expect(spanElement.text()).toBe("(박찬종)");
   });
 });
